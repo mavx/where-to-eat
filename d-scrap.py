83b0f6c7dba2c92419162d0a439692743374cbae
Swarna <SwarnaLye@iPsychs-MacBook-Pro.local>
2017-06-25 19:50:54 +0800

    Incomplete - still fucked up

--- d-scrap.py
+++ d-scrap.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import pandas as pd
 
 key=os.environ['K1']
 
@@ -8,21 +9,38 @@ ep = "https://maps.googleapis.com/maps/api/place/textsearch/json"
 
 token = True
 next_page_token = None
-d = {}
+d = []
 
-with open('dataset/database.txt', 'w') as f:
-    n = 0
-    while token and (n < 200):
-        print('Requesting #{}'.format(n))
-        r = requests.get(ep, params = {"query":"restaurants in SS15", "key":key, 'next_page_token':next_page_token})
-        token = r.json().get('next_page_token')
-        next_page_token = token
+n = 0
+while token and (n < 1):
+    print('Requesting #{}'.format(n))
+    r = requests.get(ep, params = {"query":"restaurants in SS15", "key":key, 'next_page_token':next_page_token})
+    token = r.json().get('next_page_token')
+    next_page_token = token
 
-        result = r.json().get('results')
-        if token != None:
-            for item in result:
-                f.write('{},'.format(json.dumps(item)))
-        else:
-            f.write('{}'.format(json.dumps(item)))
-        
-        n += 1
+    result = r.json().get('results')
+    d.append(result)
+    
+    n += 1
+
+# j = json.dumps(d, indent= 2)
+j = d.json()
+
+df = pd.read_json(j)
+print(df.head())
+
+# def fn1(val):
+#     return val['location']['lat']
+
+# def fn2(val):
+#     return val['location']['lng']
+
+# df['latitude'] = df['geometry'].apply(fn1, 1)
+# df['longitude'] = df['geometry'].apply(fn2, 1)
+# df2 = df.drop(['geometry'], axis = 1)
+# df2.drop(['icon'], axis = 1, inplace = True)
+# df2.drop(['photos'], axis = 1, inplace = True)
+# df2.drop(['reference'], axis = 1, inplace = True)
+# df2.drop(['id'], axis = 1, inplace = True)
+
+# df2.to_csv('dataset/df.csv')

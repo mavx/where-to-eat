0ff0355bf969028824e18d4b644a8ed0fc5fb064
Swarna <SwarnaLye@iPsychs-MacBook-Pro.local>
2017-06-25 15:49:07 +0800

    change database.txt to json

--- json_fix.py
+++ json_fix.py
@@ -4,15 +4,40 @@ import ast
 with open('database.txt', 'r') as f:
 	text = f.read()
 
-tt = text.split(']\n[')
-dict_strings = [item for item in tt if len(item) > 2]
-dicts = [ast.literal_eval(item) for item in dict_strings]
+tt = text.split('},{')
+l = len(tt)
+d = [] 
+for i in tt:
+	if i == tt[0]:
+		i = i+'}'
+		# print('First esntry processed')
+	elif i == tt[-1]:
+		i = '{'+i
+		# print('Last entry processed')
+	else:		
+		i = '{'+i+'}'
+	# 	print('Processing...')
+	# print('Converting data to json')
+	j = json.loads(i)
+	# print('Adding to dictionary')
+	d.append(j)
 
-combined_array = []
-for item in dicts:
-	combined_array.extend(item)
 
-j = json.dumps(combined_array, indent=2)
+# ttt = json.loads(tt)
 
-with open('fixed.txt', 'w') as o:
-	o.write(j)
+# dict_strings = [item for item in tt if len(item) > 2]
+# dicts = [ast.literal_eval(item) for item in dict_strings]
+
+# combined_array = []
+# for item in dicts:
+# 	combined_array.extend(item)
+
+# j = json.dumps(combined_array, indent=2)
+
+# with open('fixed.txt', 'w') as o:
+# 	o.write(ttt)
+
+j = json.dumps(d, indent= 2)
+
+with open('database.json', 'w') as fp:
+	fp.write(j)

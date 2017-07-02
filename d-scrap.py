094f388d4e22268dccffe8e9b1ddab8d464f9d0b
Swarna <SwarnaLye@iPsychs-MacBook-Pro.local>
2017-07-02 18:11:23 +0800

    Combines d-scrap+json_fix+df

--- d-scrap.py
+++ d-scrap.py
@@ -31,12 +31,16 @@ def query(token):
 
 
 token = None
+d = []
 n = 0
 while n < 5:
     j = query(token)
     token = j.get('next_page_token')
     print('RETRIEVED TOKEN:', token)
     print(j.keys())
+    results = j['results']
+
+    d.extend(results)
 
     n += 1
     print('\nPAUSING FOR 3 FREAKING SECONDS...')
@@ -44,48 +48,31 @@ while n < 5:
     if token is None:
         break
 
-raise SystemExit
-
-n = 0
-while token and (n < 2):
-    print('Requesting #{}'.format(n))
-    j = query(next_page_token)
-    print('RESULTS:', j)
-    token = j.get('next_page_token', 'NIGGA')
-    next_page_token = token
-
-    result = j.get('results')
-    d.append(result)
-    
-    n += 1
-
-raise SystemExit
-
-
 
 # j = json.dumps(d, indent= 2)
 # prsint(d[0][0]['name'])
 dd = {} #dictionary of database
-for i , x in enumerate(d[0]):
-    dd[d[0][i]['name']] = d[0][i]
+for i , x in enumerate(d):
+    dd[d[i]['name']] = d[i]
+  
 
-print(len(dd))    
+df = pd.DataFrame.from_dict(dd)
 
-# df = pd.read_json(j)
-# print(df.head())
+df = pd.DataFrame.transpose(df)
 
-# def fn1(val):
-#     return val['location']['lat']
 
-# def fn2(val):
-#     return val['location']['lng']
+def fn1(val):
+    return val['location']['lat']
 
-# df['latitude'] = df['geometry'].apply(fn1, 1)
-# df['longitude'] = df['geometry'].apply(fn2, 1)
-# df2 = df.drop(['geometry'], axis = 1)
-# df2.drop(['icon'], axis = 1, inplace = True)
-# df2.drop(['photos'], axis = 1, inplace = True)
-# df2.drop(['reference'], axis = 1, inplace = True)
-# df2.drop(['id'], axis = 1, inplace = True)
+def fn2(val):
+    return val['location']['lng']
 
-# df2.to_csv('dataset/df.csv')
+df['latitude'] = df['geometry'].apply(fn1, 1)
+df['longitude'] = df['geometry'].apply(fn2, 1)
+df2 = df.drop(['geometry'], axis = 1)
+df2.drop(['icon'], axis = 1, inplace = True)
+df2.drop(['photos'], axis = 1, inplace = True)
+df2.drop(['reference'], axis = 1, inplace = True)
+df2.drop(['id'], axis = 1, inplace = True)
+
+df2.to_csv('dataset/df.csv')

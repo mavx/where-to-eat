34810b81ba5c9d8983fbaa2f1444d62d471fccf4
Swarna <SwarnaLye@iPsychs-MacBook-Pro.local>
2017-07-02 16:05:42 +0800

    NiGGA doesn't work

--- d-scrap.py
+++ d-scrap.py
@@ -11,23 +11,47 @@ token = True
 next_page_token = None
 d = []
 
-n = 0
-while token and (n < 1):
-    print('Requesting #{}'.format(n))
-    r = requests.get(ep, params = {"query":"restaurants in SS15", "key":key, 'next_page_token':next_page_token})
-    token = r.json().get('next_page_token')
-    next_page_token = token
 
-    result = r.json().get('results')
+def query(token):
+    r = requests.get(ep, params = {"query":"restaurants in SS15", "key":key, "pagetoken":token})
+    print('SENT REQUEST:', r.url)
+    return r.json()
+
+n = 0
+while token and (n < 2):
+    print('Requesting #{}'.format(n))
+    # print(token)
+    # r = requests.get(ep, params = {"query":"restaurants in SS15", "key":key, "pagetoken":next_page_token})
+    # print('SENT REQUEST:', '\n\n{}\n\n'.format(r.url))
+    j = query(next_page_token)
+    print('RESULTS:', j)
+    # token = r.json().get('next_page_token')
+    # print(r.json().keys())
+    # print(r.json())
+    token = j.get('next_page_token', 'NIGGA')
+    next_page_token = token
+    # print(token)
+
+    result = j.get('results')
+    # print(result)
     d.append(result)
     
     n += 1
 
-# j = json.dumps(d, indent= 2)
-j = d.json()
+raise SystemExit
 
-df = pd.read_json(j)
-print(df.head())
+
+
+# j = json.dumps(d, indent= 2)
+# prsint(d[0][0]['name'])
+dd = {} #dictionary of database
+for i , x in enumerate(d[0]):
+    dd[d[0][i]['name']] = d[0][i]
+
+print(len(dd))    
+
+# df = pd.read_json(j)
+# print(df.head())
 
 # def fn1(val):
 #     return val['location']['lat']
